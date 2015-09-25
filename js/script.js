bbc123854703ed0b3ff47df9bfc46ffad3f05cc1
Elizabeth <anatskayalisa@gmail.com>
2015-09-25 21:29:03 +0300

    2nd edition

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
 /**
  * Created by Elizabeth on 22.09.2015.
  */
-
-
 var link = document.querySelector(".searchHotelBtn");
 var popup = document.querySelector(".formContent");
 
 var form = popup.querySelector("form");
 var arrivalDay = popup.querySelector("[name='arrivalDay']");
 var departureDay = popup.querySelector("[name='departureDay']");
-var formStatus =false;
+var formStatus = false;
 var map = document.querySelector(".staticMap");
+
 map.classList.add("staticMapHidden");
 
 var intFrame = document.querySelector(".frame");
+
 intFrame.classList.add("frameShow");
 
-link.addEventListener("click", function(event) {
+link.addEventListener("click", function (event) {
     event.preventDefault();
 
-    if(formStatus) {
+    if (formStatus) {
         popup.classList.remove("formContentShow");
         formStatus = false;
     }
-    else{
+    else {
         popup.classList.add("formContentShow");
         arrivalDay.focus();
         formStatus = true;
     }
 });
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
     if (!(arrivalDay.value && departureDay.value)) {
         event.preventDefault();
     } else {
@@ -38,7 +39,8 @@ form.addEventListener("submit", function(event) {
     }
 });
 
-window.addEventListener("keydown", function(event) {
+window.addEventListener("keydown", function (event) {
+    event.preventDefault();
     if (event.keyCode == 27) {
         if (popup.classList.contains("formContentShow")) {
             popup.classList.remove("formContentShow");
